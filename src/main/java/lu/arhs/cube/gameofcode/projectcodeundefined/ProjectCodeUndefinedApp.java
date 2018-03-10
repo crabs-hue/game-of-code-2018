5f46d253d9e9f8970244ab577988fbee8c69a8dd
Elio Goettelmann <elio.goettelmann@arhs-cube.com>
2018-03-10 18:40:48 +0100

    Bootstrapped SpringBoot app

---
+++ src/main/java/lu/arhs/cube/gameofcode/projectcodeundefined/ProjectCodeUndefinedApp.java
@@ -0,0 +1,14 @@
+package lu.arhs.cube.gameofcode.projectcodeundefined;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodeUndefinedApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCodeUndefinedApp.class, args);
+    }
+
+}
